0fb9d78d1a0c243bc8859cbe1faa32ff7734f963
Yudai NAKATA <yudai.nkt@gmail.com>
2023-01-31 22:44:47 +0900

    chore: adjust background color of OGP image

--- scripts/convert-ogp-image.ts
+++ scripts/convert-ogp-image.ts
@@ -2,5 +2,5 @@ import sharp from "sharp";
 
 await sharp("src/ogp-image.svg")
   .png()
-  .flatten({ background: "white" })
+  .flatten({ background: "#f6f6f6" })
   .toFile("public/ogp-image.png");
